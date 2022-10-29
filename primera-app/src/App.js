cf27db1abc9fa19d63e9d9c0443afd0a409dc2e9
tinchiusco <86133947+tinchiusco@users.noreply.github.com>
2022-10-29 10:40:31 -0300

    probando react

--- primera-app/src/App.js
+++ primera-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import Prueba from './components/Prueba';
 
 
 function App() {
   return (
     <div className="App"> 
-    <NavBar header = {"Bienvenidos a la pagina de comercio de esclavos."} cont = {100} />
+    <NavBar/>
     <ItemListContainer greeting={"Bienvenidos al e-commerce de videojuegos"}/>
+    <Prueba saludo1/>
     
     </div>
   );
---
+++ primera-app/src/components/Prueba/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles.css";
+
+const Prueba = (saludo1) => {
+  return (
+    <div className='saludo1'><p>
+            {saludo1}
+        </p>
+        
+    </div>
+  )
+}
+
+export default Prueba
